a1b941f9a94f28da88a49df9928ec9733c23beb3
Verónica Vidal <duberneylopezlopez1@gmail.com>
2024-07-31 22:13:04 -0500

    funciones

---
+++ bucles.py
@@ -0,0 +1,42 @@
+#romper un bucle
+
+
+"""MAX_GREETS = 4
+
+num_greets = 0
+want_greet = 'S'
+
+while want_greet == 'S':
+    print('Hola qué tal!')
+    num_greets += 1
+    if num_greets == MAX_GREETS:
+        print('Máximo número de saludos alcanzado')
+        break
+    want_greet = input('¿Quiere otro saludo? [S/N] ')
+print('Que tenga un buen día')"""
+
+#senterncia for 
+"""word = 'Python'
+
+for letter in word:
+    print(letter)"""
+    
+#sentencia de numeros
+
+"""for i in range(0, 3):
+    print(i)
+
+for i in range(3):  # No hace falta indicar el inicio si es 0
+    print(i)"""
+    
+"""for _ in range(10):
+    print('Repeat me 10 times!')
+
+"""
+
+#bucle anidados
+
+"""for num_table in range(1, 10):
+    for mul_factor in range(1, 10):
+        result = num_table * mul_factor
+        print(f'{num_table} * {mul_factor} = {result}')"""
---
+++ listas.py
@@ -0,0 +1,51 @@
+#Creando listas
+
+"""empty_list = []
+
+languages = ['Python', 'Ruby', 'Javascript']
+
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13]
+
+data = ['Tenerife', {'cielo': 'limpio', 'temp': 24}, 3718, (28.2933947, -16.5226597)]"""
+
+#Operaciones con listas
+
+"""shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping[0]
+
+shopping[1]
+
+shopping[2]
+
+shopping[-1]  # acceso con índice negativo
+    """
+
+"""shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.append('Atún')
+
+shopping
+    """
+    
+    
+"""even_numbers = []
+
+for i in range(20):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+print(even_numbers)"""
+
+
+#Añadir en cualquier posición de una lista
+"""shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping.insert(1, 'Jamón')
+
+
+
+shopping.insert(3, 'Queso')"""
+
+
+

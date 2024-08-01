de4cfd685644c0d934bf08c2c191bf2933c4ffb4
Verónica Vidal <duberneylopezlopez1@gmail.com>
2024-07-31 20:53:41 -0500

    31/julio

---
+++ Ejercicio 0.py
@@ -0,0 +1,113 @@
+#help("keywords")
+#num1=type(9.6)
+#print(num1)
+
+#num1= height=9564
+#print(num1)
+
+#result= 10* 3.0
+#print(result)
+#result =type(result)
+
+
+#num1=10
+#num2=3
+#result=num1+num2
+#result=num1-num2
+#result=num1*num2
+#result=num1/num2
+#result=num1//num2
+#result=num1%num2
+
+#print(result)
+
+#total_cars = 100
+#sold_cars = 20
+#total_cars = total_cars - sold_cars
+#print(total_cars)
+
+#num1=10
+#num2=3
+#result=num1num2
+
+#num1=10
+#num2=3
+#result=num1/num2
+#result=int(result)
+#print(str(result))
+
+'''msg = 'Primera línea\nSegunda línea\nTercera línea'
+print(msg)'''
+
+'''msg = 'Valor = \t40'
+print(msg)'''
+
+'''msg = 'Necesitamos \'escapar\' la comilla simple'
+print(msg)'''
+
+'''msg = 'Capítulo \\ Sección \\ Encabezado'
+print(msg)'''
+
+'''text = 'abc\ndef'
+print(text)
+
+text = r'abc\ndef'
+print(text)
+
+text = 'a\tb\tc'
+print(text)
+
+text = r'a\tb\tc'
+print(text)'''
+
+'''msg1 = '¿Sabes por qué estoy acá?'
+msg2 = 'Porque me apasiona'
+
+print(msg1, msg2)
+
+print(msg1, msg2, sep='|')
+
+print(msg2, end='!!')'''
+
+
+'''name = input('Introduzca su nombre: ')
+age = input('Introduzca su edad: ')
+print('Hola mucho gusto soy ', + name + 'y tengo', + age)'''
+
+
+'''value1 = int(input('Ingrese un numero: '))
+value2 = int(input('Ingrese otro numero: '))
+
+result = value1 + value2
+print(value1, value2, sep='+', end='=')
+print(result)
+
+result = value1 - value2
+print(value1, value2, sep='-', end='=')
+print(result)
+
+result = value1 * value2
+print(value1, value2, sep='*', end='=')
+print(result)
+
+result = value1 / value2
+print(value1, value2, sep='/', end='=')
+print(result)'''
+
+
+'''reaction = 'Wow'
+
+print(reaction * 4)'''
+
+'''sentence = 'Hola, Mundo'
+
+print (sentence[0])
+print(sentence[-1])
+print(sentence[4])
+print(sentence[-5])'''
+
+name = 'Elon Musk'
+age = 49
+fortune = 43_300
+
+print(f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones')
